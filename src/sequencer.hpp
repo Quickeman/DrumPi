4d317110a3f4f91a2115ec4d6caa7805d2ae8a6f
Finn Quicke <Quickeman@github.com>
2021-02-13 23:49:15 +0000

    Created Sequencer8 & -16 step classes; SequenceStep class

--- src/sequencer.hpp
+++ src/sequencer.hpp
@@ -3,11 +3,55 @@
 #ifndef sequencer_h
 #define sequencer_h
 
+#define NUM_DRUMS
+
+class SequenceStep {
+    public:
+        /** Step constructor
+         */
+        SequenceStep();
+
+        /** Adds the sample with the passed ID to the step
+         */
+        void addToStep(int id);
+
+        /** Removes the sample with the passed ID from the step
+         */
+        void removeFromStep(int id);
+
+        /** Returns true if the sample with the passed ID is active in the step
+         */
+        bool isActive(int id);
+
+        /** Returns the number of active samples in step
+         */
+        int numActive();
+    
+    private:
+        bool switches[NUM_DRUMS];
+};
+
 template<int NUM_STEPS>
 class Sequencer {
     public:
+        /** Sequencer constructor.
+         */
         Sequencer();
+        /** Sequencer destructor.
+         */
         ~Sequencer();
+
+        /** Returns the current step number.
+         */
+        int currentStep();
+    
+    private:
+        /** Ticker function.
+        Called to update the Sequencer's step tracker
+        */
+        void tick();
+
+        SequenceStep steps[NUM_STEPS];
 };
 
 class Sequencer8Step : Sequencer<8> {};
