0b2d90d84df44867cfd455bc205f67eba5a10921
Finn Quicke <Quickeman@github.com>
2021-02-14 14:45:59 +0000

    Added methods and attributes to Sequencer

--- src/sequencer.hpp
+++ src/sequencer.hpp
@@ -1,61 +1,84 @@
 // File: sequencer.hpp
+#pragma once
+#include <vector>
+
+namespace sequencer {
 
 #ifndef sequencer_h
 #define sequencer_h
 
-#define NUM_DRUMS
+#ifndef NUM_DRUMS
+#define NUM_DRUMS 8
+#endif // define NUM_DRUMS
 
-class SequenceStep {
+/*! Step class for a Sequencer object. */
+class Step {
     public:
-        /** Step constructor
-         */
-        SequenceStep();
+        /*! Step constructor. */
+        Step();
 
-        /** Adds the sample with the passed ID to the step
-         */
+        /*! Adds the sample with the passed ID to the step. */
         void addToStep(int id);
 
-        /** Removes the sample with the passed ID from the step
-         */
+        /*! Removes the sample with the passed ID from the step. */
         void removeFromStep(int id);
 
-        /** Returns true if the sample with the passed ID is active in the step
-         */
+        /*! Returns true if the sample with the passed ID is active in the step. */
         bool isActive(int id);
 
-        /** Returns the number of active samples in step
-         */
+        /*! Returns the number of active samples in step. */
         int numActive();
     
     private:
+        /*! Container for drum trigger switches. */
         bool switches[NUM_DRUMS];
 };
 
-template<int NUM_STEPS>
+
 class Sequencer {
     public:
-        /** Sequencer constructor.
-         */
-        Sequencer();
-        /** Sequencer destructor.
-         */
+        /*! Sequencer constructor. */
+        Sequencer(const int numSteps);
+        /*! Sequencer destructor. */
         ~Sequencer();
 
-        /** Returns the current step number.
-         */
-        int currentStep();
+        /*! Advance the sequencer by one step.
+        Updates tracking and sends relevant samples to output. */
+        void advance();
+
+        /*! Returns the current step number. */
+        int getCurrentStep();
+
+        /*! Starts the sequencer.
+        If already running, resets playhead to beginning of sequence. */
+        void start();
+
+        /*! Sets the clock rate in BPM. */
+        void setRate_bpm(int bpm);
+        /*! Sets the clock rate in miliseconds. */
+        void setRate_ms(int time);
     
     private:
-        /** Ticker function.
-        Called to update the Sequencer's step tracker
-        */
-        void tick();
+        /*! Container for step objects. */
+        std::vector<Step> steps;
+        /*! Number of steps in the sequence. */
+        int numSteps;
 
-        SequenceStep steps[NUM_STEPS];
+        /*! ID of the current step.
+        Index in the `steps` array. */
+        int stepNum;
+        /*! Call to update the active step ID. */
+        void _updateStepID();
+
+        /*! Pointer to currently active step object. */
+        Step* currentStep;
+        /*! Call to update the active step pointer. */
+        void _updateStepPtr();
+
+        /*! Updates both the active step ID and pointer. */
+        void updateStep();
 };
 
-class Sequencer8Step : Sequencer<8> {};
-
-class Sequencer16Step : Sequencer<16> {};
-
 #endif // define sequencer_h
+
+} // namespace sequencer
