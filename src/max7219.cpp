3bd37b785d8a2386d77f84d3cc8c84b2149d4c7d
murdograham <2261517G@student.gla.ac.uk>
2021-02-19 14:45:15 +0000

    getters, command and write methods

--- src/max7219.cpp
+++ src/max7219.cpp
@@ -1,5 +1,22 @@
+#define MAX7219_REG_NOOP 0x0
+#define MAX7219_REG_DIGIT0 0x1
+#define MAX7219_REG_DIGIT1 0x2
+#define MAX7219_REG_DIGIT2 0x3
+#define MAX7219_REG_DIGIT3 0x4
+#define MAX7219_REG_DIGIT4 0x5
+#define MAX7219_REG_DIGIT5 0x6
+#define MAX7219_REG_DIGIT6 0x7
+#define MAX7219_REG_DIGIT7 0x8
+#define MAX7219_REG_DECODEMODE 0x9
+#define MAX7219_REG_INTENSITY 0xA
+#define MAX7219_REG_SCANLIMIT 0xB
+#define MAX7219_REG_SHUTDOWN 0xC0
+#define MAX7219_REG_DISPLAYTEST 0xF
+
 #include "max7219.hpp"
+extern "C" {
 #include <wiringPiSPI.h>
+}
 
 Max7219::Max7219(unsigned char decodeMode,
                 unsigned char intensity,
@@ -12,12 +29,38 @@ Max7219::Max7219(unsigned char decodeMode,
     shutdown(shutdown),
     displayTest(displayTest)
 {
-    int init = wiringPiSPISetup(0, 500000);
+    wiringPiSPISetup(0, 500000);
+    command(MAX7219_REG_DECODEMODE, decodeMode);
+    command(MAX7219_REG_INTENSITY, decodeMode);
+    command(MAX7219_REG_SCANLIMIT, decodeMode);
+    command(MAX7219_REG_SHUTDOWN, decodeMode);
+    command(MAX7219_REG_DISPLAYTEST, decodeMode);
 }
 
 Max7219::~Max7219() {
 }
 
+unsigned char Max7219::getDecodeMode(){
+    return decodeMode;
+}
+unsigned char Max7219::getIntensity(){
+    return intensity;
+}
+unsigned char Max7219::getScanLimit(){
+    return scanLimit;
+}
+unsigned char Max7219::getShutdown(){
+    return shutdown;
+}
+unsigned char Max7219::getDisplayTest(){
+    return displayTest;
+}
+
 void Max7219::write(unsigned char* data, unsigned int len) {
     wiringPiSPIDataRW(0, data, len);
+}
+
+void Max7219::command(unsigned char reg, unsigned char data) {
+    unsigned char tx[2] = {reg, data};
+    write(tx, 2);
 }
--- src/max7219.hpp
+++ src/max7219.hpp
@@ -1,19 +1,9 @@
-#define MAX7219_REG_NOOP = 0x0
-#define MAX7219_REG_DIGIT0 = 0x1
-#define MAX7219_REG_DIGIT1 = 0x2
-#define MAX7219_REG_DIGIT2 = 0x3
-#define MAX7219_REG_DIGIT3 = 0x4
-#define MAX7219_REG_DIGIT4 = 0x5
-#define MAX7219_REG_DIGIT5 = 0x6
-#define MAX7219_REG_DIGIT6 = 0x7
-#define MAX7219_REG_DIGIT7 = 0x8
-#define MAX7219_REG_DECODEMODE = 0x9
-#define MAX7219_REG_INTENSITY = 0xA
-#define MAX7219_REG_SCANLIMIT = 0xB
-#define MAX7219_REG_SHUTDOWN = 0xC0
-#define MAX7219_REG_DISPLAYTEST = 0xF
 
 #include <stdint.h>
+
+extern "C" {
+#include <wiringPiSPI.h>
+}
 
 class Max7219
 {
@@ -25,6 +15,8 @@ class Max7219
                 shutdown,
                 displayTest;
         void write(unsigned char* data, unsigned int len);
+        void command(uint8_t reg, uint8_t data);
+
     public:
         Max7219(unsigned char decodeMode = 0x00,
                 unsigned char intensity = 0x07,
@@ -42,15 +34,14 @@ class Max7219
         void setDisplayTest(uint8_t value);
 
         // Getters
-        void getByte(uint8_t digit);
-        void getDecodeMode();
-        void getIntensity();
-        void getScanLimit();
-        void getShutdown();
-        void getDisplayTest();
+        unsigned char getDigit(unsigned char digit);
+        unsigned char getDecodeMode();
+        unsigned char getIntensity();
+        unsigned char getScanLimit();
+        unsigned char getShutdown();
+        unsigned char getDisplayTest();
 
         // Actions
         void flush();
         void clear();
-        void command(uint8_t reg, uint8_t data);
 };
