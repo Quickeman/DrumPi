3ca7a0430a81af5d4591af65cf7cfeb693317ec3
Finn Quicke <Quickeman@github.com>
2021-02-20 18:11:31 +0000

    Added Timer and Metronome tests, generalised Clock tests

--- test/test_clock.cpp
+++ test/test_clock.cpp
@@ -19,25 +19,26 @@ BOOST_AUTO_TEST_CASE(constructors) {
 BOOST_AUTO_TEST_CASE(rateSetting) {
     // Test the clock rate setting & retrieving
     Clock c;
-    int r = 500; // rate = 500ms
+    int r = 100; // rate = 100ms
 
     c.setRate(r);
     
     BOOST_CHECK_EQUAL(r, c.getRate());
 }
 
+class TestClock1 : public Clock {
+    public:
+    int i = 0;
+    void tick() override {
+        i++;
+    }
+};
+
 BOOST_AUTO_TEST_CASE(counter) {
     // Test the clocking with a basic ticker counter
-    class TestClock : public Clock {
-        public:
-        int i = 0;
-        void tick() override {
-            i++;
-        }
-    };
-    TestClock c;
-    int r = 100; // rate = 100ms
-    c.setRate(100);
+    TestClock1 c;
+    int r = 10; // rate = 10ms
+    c.setRate(r);
 
     BOOST_CHECK_EQUAL(0, c.i);
 
@@ -45,35 +46,28 @@ BOOST_AUTO_TEST_CASE(counter) {
 
     BOOST_CHECK_EQUAL(1, c.i);
 
-    std::this_thread::sleep_for(std::chrono::milliseconds(110));
+    std::this_thread::sleep_for(std::chrono::milliseconds(r + (r/10)));
 
     BOOST_CHECK_EQUAL(2, c.i);
 
-    std::this_thread::sleep_for(std::chrono::milliseconds(110));
+    std::this_thread::sleep_for(std::chrono::milliseconds(r + (r/10)));
 
     BOOST_CHECK_EQUAL(3, c.i);
 }
 
 BOOST_AUTO_TEST_CASE(stopping) {
     // Test the clock stops when requested
-    class TestClock : public Clock {
-        public:
-        int i = 0;
-        void tick() override {
-            i++;
-        }
-    };
-    TestClock c;
-    int r = 10; // rate = 100ms
+    TestClock1 c;
+    int r = 10; // rate = 10ms
     c.setRate(r);
     c.start();
 
-    std::this_thread::sleep_for(std::chrono::milliseconds(25));
+    std::this_thread::sleep_for(std::chrono::milliseconds((2*r) + (r / 2)));
 
     int val = c.i;
     c.stop();
 
-    std::this_thread::sleep_for(std::chrono::milliseconds(25));
+    std::this_thread::sleep_for(std::chrono::milliseconds((2*r) + (r / 2)));
 
     BOOST_CHECK_EQUAL(val, c.i);
 }
