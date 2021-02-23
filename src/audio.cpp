165f7c544342321aeb7412836602d78f0e2d0b5e
Finn Quicke <Quickeman@github.com>
2021-02-23 17:27:43 +0000

    Regressed JackDriver to AudioDriver, is a C++ wrapper object for JACK

--- src/audio.cpp
+++ src/audio.cpp
@@ -4,10 +4,22 @@
 using namespace drumpi;
 using namespace audio;
 
-JackDriver::JackDriver() {
+AudioEngine::AudioEngine() {
 
 }
 
-AudioDriver::AudioDriver() {
+int AudioEngine::setup(std::string clientName, std::string serverName) {
+
+}
+
+void AudioEngine::start(std::vector<SAMPLE_FORMAT> (*_callback)(int)) {
+
+}
+
+int AudioEngine::_read(jack_nframes_t nFrames, void *arg) {
+
+}
+
+void AudioEngine::_shutdown(void *arg) {
     
 }
--- src/audio.hpp
+++ src/audio.hpp
@@ -2,52 +2,61 @@
 #ifndef DRUMPI_AUDIO_H
 #define DRUMPI_AUDIO_H
 
-#include <jack/jack.h>
 #include <vector>
+#include <string>
+
+#include <jack/jack.h>
 
 namespace drumpi {
 namespace audio {
 
+/*! Sample data format used by JACK (`float`). */
+typedef jack_default_audio_sample_t SAMPLE_FORMAT;
+
 /*! Audio driver class for interacting with the JACK server. */
-class JackDriver {
+class AudioEngine {
     public:
         /*! Constructor.
         Sets up the connection to the JACK server. */
-        JackDriver();
+        AudioEngine();
 
         /*! Setup method.
-        Used to specify parameters to JACK. */
-        int setup();
+        Used to specify parameters to JACK.
+        \param clientName requested client name in JACK.
+        \param serverName requested server name in JACK. `NULL` by default.
+        \return error code, zero means no error. */
+        int setup(std::string clientName, std::string serverName = NULL);
 
-        /*! Informs JACK that the program is ready to go. */
-        void start();
+        /*! Informs JACK that the program is ready to go.
+        \param _callback pointer to a callback function to collect samples. */
+        void start(std::vector<SAMPLE_FORMAT> (*_callback)(int));
 
         /*! Read method to send output buffer to the JACK server.
-        Called by JACK when samples are needed. */
-        int read(jack_nframes_t numFrames, void *arg);
+        Called by JACK when samples are needed.
+        \param nFrames number of frames requested by JACK.
+        \param arg */
+        int _read(jack_nframes_t nFrames, void *arg);
 
         /*! Shutdown method to exit the program should the JACK server shut down or disconnect the client. */
-        void shutdown(void *arg);
-
-        /*! Send samples to the output buffer.
-        \param samples a vector of the samples to be sent to the output. 2D to allow for non-monophonic output. */
-        int toOutput(std::vector<std::vector<jack_default_audio_sample_t>> samples);
+        void _shutdown(void *arg);
     
     private:
-        /*! Output sample buffer. */
-        std::vector<std::vector<jack_default_audio_sample_t>> buffer;
-};
+        /*! Pointer to callback function for fetching samples. */
+        std::vector<SAMPLE_FORMAT> (*_callback)(int);
 
-
-/*! Audio driver class specifically for use with the DrumPi. Higher-level than JackDriver. */
-class AudioDriver {
-    public:
-        /*! Constructor. */
-        AudioDriver();
-
-    private:
-        /*! JackDriver object. */
-        JackDriver jackD;
+        /*! Pointer to the JACK client. */
+        jack_client_t *client;
+        /*! JACK client name. */
+        std::string clientName;
+        /*! JACK server name. */
+        std::string serverName;
+        /*! JACK ports. */
+        std::vector<jack_port_t*> outPorts;
+        std::vector<std::string> ports;
+        /*! JACK options. */
+        jack_options_t options = JackNullOption;
+        /*! JACK status. */
+        jack_status_t status;
 };
 
 } // namespace audio
