1131d5b6af6dc79295100e538610a56425fb1e2f
Finn Quicke <Quickeman@github.com>
2021-02-23 18:02:58 +0000

    Created PlaybackEngine class to manage sample playback

---
+++ src/playback.cpp
@@ -0,0 +1,24 @@
+#include "playback.hpp"
+
+using namespace drumpi;
+using namespace audio;
+
+template<typename T>
+PlaybackEngine<T>::PlaybackEngine() {
+
+}
+
+template<typename T>
+std::vector<T> PlaybackEngine<T>::getSamples(int nSamples) {
+
+}
+
+template<typename T>
+void PlaybackEngine<T>::addToOutput(int id) {
+
+}
+
+template<typename T>
+void PlaybackEngine<T>::removeFromOutput(int id) {
+    
+}
---
+++ src/playback.hpp
@@ -0,0 +1,49 @@
+// File: playback.hpp
+#ifndef DRUMPI_PLAYBACK_H
+#define DRUMPI_PLAYBACK_H
+
+#include <vector>
+
+#include "audio.hpp"
+#include "sampleSource.hpp"
+
+namespace drumpi {
+namespace audio {
+
+/*! Sample handling class.
+Manages audio clips for sending to output. */
+template<typename T = SAMPLE_FORMAT>
+class PlaybackEngine {
+    public:
+        /*! Constructor. */
+        PlaybackEngine();
+
+        /*! Retrieves samples.
+        \param nSamples number of samples to return.
+        \return a buffer of samples. */
+        std::vector<T> getSamples(int nSamples);
+
+        /*! Adds the specified drum to the output engine.
+        \param id ID of the drum to add. */
+        void addToOutput(int id);
+
+        /*! Removes the specified drum sample from the output.
+        \param id ID of the drum to remove. */
+        void removeFromOutput(int id);
+    
+    private:
+        /*! SampleSource object pointers. */
+        std::vector<SampleSource<T>> sources;
+        /*! Switches to store whether each source is being played. */
+        std::vector<bool> switches;
+        /*! Types of sample retrievers. */
+        std::vector<sampleSourceType_t> types;
+
+        /*! Counter storing the number of active clips. */
+        int numActive;
+};
+
+} // namespace audio
+} // namespace drumpi
+
+#endif // define DRUMPI_PLAYBACK_H
