02bb7b8e832d6f45fe6a7be2e8436cb7eaffb8d3
Finn Quicke <Quickeman@github.com>
2021-02-24 16:05:30 +0000

    Added drumpi namespace to sequencer cpp and test files

--- src/sequencer.cpp
+++ src/sequencer.cpp
@@ -2,7 +2,8 @@
 
 #include "sequencer.hpp"
 
-namespace sequencer {
+using namespace drumpi;
+using namespace sequencer;
 
 // Step class
 
@@ -104,5 +105,3 @@ void Sequencer::_updateStepID() {
 void Sequencer::_updateStepPtr() {
     currentStep = &steps[stepNum];
 }
-
-} // namespace sequencer
