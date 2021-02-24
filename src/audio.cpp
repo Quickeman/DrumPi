456e0e45ea43ba6d5ba1f6612f8989d03aacea8c
Finn Quicke <Quickeman@github.com>
2021-02-24 12:44:23 +0000

    Refactored AudioEngine to be thread-safe for JACK

--- src/audio.cpp
+++ src/audio.cpp
@@ -3,23 +3,28 @@
 
 using namespace drumpi;
 using namespace audio;
+using namespace engine;
 
 AudioEngine::AudioEngine() {
 
 }
 
-int AudioEngine::setup(std::string clientName, std::string serverName) {
-
+int AudioEngine::setup(PlaybackEngine<SAMPLE_FORMAT>& pbe, std::string clientName, std::string serverName) {
+    return -1;
 }
 
-void AudioEngine::start(std::vector<SAMPLE_FORMAT> (*_callback)(int)) {
-
+int AudioEngine::start() {
+    return -1;
 }
 
-int AudioEngine::_read(jack_nframes_t nFrames, void *arg) {
+int AudioEngine::stop() {
+    return -1;
+}
 
+int AudioEngine::_process(jack_nframes_t nFrames, void *arg) {
+    return -1;
 }
 
 void AudioEngine::_shutdown(void *arg) {
-    
+
 }
--- src/audio.hpp
+++ src/audio.hpp
@@ -7,43 +7,49 @@
 
 #include <jack/jack.h>
 
+#include "defs.hpp"
+#include "playback.hpp"
+
 namespace drumpi {
 namespace audio {
+namespace engine {
 
-/*! Sample data format used by JACK (`float`). */
-typedef jack_default_audio_sample_t SAMPLE_FORMAT;
-
-/*! Audio driver class for interacting with the JACK server. */
+/*! Audio engine class for interacting with the JACK server. */
 class AudioEngine {
     public:
-        /*! Constructor.
-        Sets up the connection to the JACK server. */
+        /*! Constructor. */
         AudioEngine();
+
+        /*! Destructor.
+        Closes the JACK client. */
+        ~AudioEngine();
 
         /*! Setup method.
         Used to specify parameters to JACK.
+        \param pbe `PlaybackEngine` type object used to retieve samples.
         \param clientName requested client name in JACK.
         \param serverName requested server name in JACK. `NULL` by default.
         \return error code, zero means no error. */
-        int setup(std::string clientName, std::string serverName = NULL);
+        int setup(PlaybackEngine<SAMPLE_FORMAT>& pbe, std::string clientName, std::string serverName = NULL);
 
         /*! Informs JACK that the program is ready to go.
-        \param _callback pointer to a callback function to collect samples. */
-        void start(std::vector<SAMPLE_FORMAT> (*_callback)(int));
+        \return error code. */
+        int start();
+
+        /*! Stops the JACK engine.
+        \return error code. */
+        int stop();
 
         /*! Read method to send output buffer to the JACK server.
         Called by JACK when samples are needed.
         \param nFrames number of frames requested by JACK.
-        \param arg */
-        int _read(jack_nframes_t nFrames, void *arg);
+        \param arg pointer to the `PlaybackEngine` object being used. */
+        static int _process(jack_nframes_t nFrames, void *arg);
 
         /*! Shutdown method to exit the program should the JACK server shut down or disconnect the client. */
-        void _shutdown(void *arg);
+        static void _shutdown(void *arg);
     
     private:
-        /*! Pointer to callback function for fetching samples. */
-        std::vector<SAMPLE_FORMAT> (*_callback)(int);
-
         /*! Pointer to the JACK client. */
         jack_client_t *client;
         /*! JACK client name. */
@@ -59,6 +65,7 @@ class AudioEngine {
         jack_status_t status;
 };
 
+} // namespace engine
 } // namespace audio
 } // namespace drumpi
 
