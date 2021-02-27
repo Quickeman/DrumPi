31cb7dd32d8505d7d7c195451194962817e8e2cd
Quickeman <finnquicke@hotmail.com>
2021-02-27 23:30:29 +0000

    Implemented most sample source class methods; .wav file reading still TODO

--- src/sampleSource.cpp
+++ src/sampleSource.cpp
@@ -4,7 +4,8 @@ using namespace drumpi;
 using namespace audio;
 
 SampleSource::SampleSource() {
-
+    status = SOURCE_ERROR;
+    type = SOURCE_GENERALISED;
 }
 
 sampleSourceStatus_t SampleSource::getStatus() {
@@ -17,28 +18,71 @@ sampleSourceType_t SampleSource::getType() {
 
 
 AudioClip::AudioClip(std::string filepath) {
+    type = SOURCE_PREGENERATED;
+    this->filepath = filepath;
 
+    loadFile();
+
+    numSamples = clip.size();
+
+    reset();
 }
 
 std::vector<sample_t> AudioClip::getSamples(int nSamples) {
-    std::vector<sample_t> b;
+    std::vector<sample_t> b(nSamples);
+    bool endFlag = false;
+    // Number of samples to copy
+    int nSamplesCopy = nSamples;
+
+    status = SOURCE_ACTIVE;
+    
+    if ((playhead + nSamples) >= numSamples) { // If the playhead will overrun...
+        endFlag = true;
+        nSamplesCopy = samplesRemaining();
+    }
+
+    for (int i = 0; i < nSamplesCopy; i++) {
+        // Copy samples from clip
+        b[i] = clip[playhead];
+        playhead++;
+    }
+
+    for (int i = nSamplesCopy; i < nSamples; i++) {
+        // Fill remainder of buffer with zeros if needed
+        b[i] = 0.f;
+    }
+
+    if (endFlag) status = SOURCE_FINISHED;
+
     return b;
 }
 
 void AudioClip::reset() {
-
+    playhead = 0;
+    updateStatus();
 }
 
 void AudioClip::updateStatus() {
-    
+    if ((playhead == 0) && !clip.empty()) {
+        status = SOURCE_READY;
+    } else
+    if (playhead >= numSamples) {
+        status = SOURCE_FINISHED;
+    } else
+    if (playhead > 0) {
+        status = SOURCE_ACTIVE;
+    } else {
+        status = SOURCE_ERROR;
+    }
 }
 
 void AudioClip::hardReset() {
-
+    loadFile();
+    reset();
 }
 
 void AudioClip::loadFile() {
-
+    // TODO
 }
 
 int AudioClip::samplesRemaining() {
