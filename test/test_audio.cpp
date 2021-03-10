eb7ff6e24a40eb70d7f99d86577dc739f4353f36
Quickeman <finnquicke@hotmail.com>
2021-03-10 16:16:38 +0000

    Refactored audio unit test to only start one instance of JACK client

--- test/test_audio.cpp
+++ test/test_audio.cpp
@@ -10,81 +10,39 @@ using namespace drumpi;
 using namespace audio;
 using namespace engine;
 
-BOOST_AUTO_TEST_CASE(constructors) {
-    // Test for NULL object references
-    AudioEngine e1;
-    AudioEngine e2;
-
-    BOOST_CHECK(&e1);
-    BOOST_CHECK(&e2);
-}
-
-class TestCallback1 : public AudioCallback {
-    std::vector<sample_t> getSamples(int nSamples) override {
-        std::vector<sample_t> v(nSamples);
-        return v;
-    }
+class TestCallback : public AudioCallback {
+    public:
+        std::vector<sample_t> getSamples(int nSamples) override {
+            std::vector<sample_t> v;
+            v.resize(nSamples);
+            for(int i = 0; i < nSamples; i++) {
+            phase += 0.01f;
+            if (phase >= 1.f) phase -= 1.f;
+            v[i] = sinf(2.f * M_PI * phase);
+            }
+            return v;
+        }
+    
+    private:
+	    float phase = 0.f;
 };
 
-BOOST_AUTO_TEST_CASE(setup) {
-    // Test the JACK setup routine
+BOOST_AUTO_TEST_CASE(audioTest) {
     AudioEngine e;
-    TestCallback1 c;
-
-    // Error codes from setup()
-    int err = e.setup(c, "DrumPiClient_test1");
+    TestCallback c;
+    int err;
+    
+    BOOST_CHECK(&e);
+    BOOST_CHECK(&c);
+    
+    err = e.setup(c, "DrumPiClient_test");
     BOOST_CHECK(!err);
-
+    
     err = e.start();
     BOOST_CHECK(!err);
-
+    
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+    
     err = e.stop();
     BOOST_CHECK(!err);
-}
-
-class TestCallback2 : public AudioCallback {
-    public:
-    int i = 0;
-    std::vector<sample_t> getSamples(int nSamples) override {
-        std::vector<sample_t> v;
-        v.resize(nSamples);
-        i++;
-        return v;
-    }
-};
-
-BOOST_AUTO_TEST_CASE(running) {
-    // Test JACK runs
-    AudioEngine e;
-    TestCallback2 c;
-
-    e.setup(c, "DrumPiClient_test2");
-    e.start();
-
-    std::this_thread::sleep_for(std::chrono::milliseconds(50));
-
-    BOOST_CHECK(c.i);
-    e.stop();
-}
-
-class TestCallback3 : public AudioCallback {
-    std::vector<sample_t> getSamples(int nSamples) override {
-        std::vector<sample_t> v;
-        v.resize(nSamples);
-        for(int i = 0; i < nSamples; i++) v[i] = 0.2f * float(rand()) / float(RAND_MAX);
-        return v;
-    }
-};
-
-BOOST_AUTO_TEST_CASE(output) {
-    // Play whitenoise for one second to physically test output
-    AudioEngine e;
-    TestCallback3 c;
-
-    e.setup(c, "DrumPiClient_test3");
-    e.start();
-
-    std::this_thread::sleep_for(std::chrono::seconds(1));
-
-    e.stop();
 }
