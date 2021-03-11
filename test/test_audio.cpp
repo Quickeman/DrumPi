808450d416c5f5da3a45b2154789b27c39b1d156
Quickeman <finnquicke@hotmail.com>
2021-03-11 17:34:00 +0000

    Added some extra AudioDriver callback tests and tidied up

--- test/test_audio.cpp
+++ test/test_audio.cpp
@@ -13,15 +13,25 @@ using namespace engine;
 class TestCallback : public AudioCallback {
     public:
         std::vector<sample_t> getSamples(int nSamples) override {
-            std::vector<sample_t> v;
+            v.clear();
             v.resize(nSamples);
+
             for(int i = 0; i < nSamples; i++) {
-            phase += 0.01f;
-            if (phase >= 1.f) phase -= 1.f;
-            v[i] = sinf(2.f * M_PI * phase);
+                phase += 0.01f;
+
+                if (phase > 1.f) phase -= 1.f;
+
+                v[i] = 0.25f * sinf(2.f * M_PI * phase);
             }
+
+            count++;
+
             return v;
         }
+
+        std::vector<sample_t> v;
+
+        int count = 0;
     
     private:
 	    float phase = 0.f;
@@ -41,6 +51,17 @@ BOOST_AUTO_TEST_CASE(audioTest) {
     err = e.start();
     BOOST_CHECK(!err);
     
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+
+    BOOST_CHECK(c.count != 0);
+
+    std::vector<sample_t> v = c.v;
+    sample_t x = 0.f;
+    for (int i = 0; i < 5; i++) {
+        x += v[i];
+    }
+    BOOST_CHECK(x != 0.f);
+
     std::this_thread::sleep_for(std::chrono::seconds(1));
     
     err = e.stop();
