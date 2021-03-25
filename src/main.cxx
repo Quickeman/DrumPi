f22a525a5d990d97b81f15f90e7150c996528091
Quickeman <finnquicke@hotmail.com>
2021-03-25 19:03:39 +0000

    Added Application to main

--- src/main.cxx
+++ src/main.cxx
@@ -1,6 +1,13 @@
 #include <cstdio>
+#include "application.hpp"
+
+using namespace drumpi;
 
 int main(int argc, char* argv[]){
     printf("Hello, DrumPi \n");
+
+    Application app;
+    app.run();
+
     return 0;
 }
