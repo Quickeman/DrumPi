628fd33ddb60013c12ffa994c27c61cc53ccf2c0
murdograham <2261517G@student.gla.ac.uk>
2021-03-29 11:59:16 +0100

    Automated project version printing

--- src/main.cxx
+++ src/main.cxx
@@ -1,11 +1,12 @@
 #include <cstdio>
+#include <iostream>
+#include "config.h.in"
 #include "application.hpp"
 
 using namespace drumpi;
 
 int main(int argc, char* argv[]){
-    printf("Hello, DrumPi \n");
-
+    std::cout << std::endl << PROJECT_NAME << " version: " << PROJECT_VER << std::endl;
     Application app;
     app.run();
 
