c328c7a99743eb4078aa1cec033ac11c96b7f055
Quickeman <finnquicke@hotmail.com>
2021-04-05 17:25:46 +0100

    Fixed #15 - Ctrl-C will now cause the DrumPi to exit normally

--- src/main.cxx
+++ src/main.cxx
@@ -1,12 +1,26 @@
-#include <iostream>
 #include "application.hpp"
 
+#include <iostream>
+#include <functional>
+
 using namespace drumpi;
+
+std::function<void(int)> shutdownHandler;
+void signalHandler(int signal) { shutdownHandler(signal); }
 
 int main(int argc, char* argv[]){
     std::cout << "DrumPi v0.1" << std::endl;
 
+    Application* appPtr;
+    signal(SIGINT, signalHandler);
+    shutdownHandler = [&](int signal) {
+        std::cout << "DrumPi: caught signal " << signal << std::endl;
+        appPtr->running = false;
+    };
+
     Application app;
+    appPtr = &app;
+    
     app.run();
 
     return 0;
